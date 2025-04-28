6998a6bcdb8cccaed4a021ce97cfb7f3db8b22d1
Darhaf <fhv.huseynov@gmail.com>
2025-04-29 00:25:47 +0200

    ajout fonction récupération des articles maj

--- api/ApiConnector.php
+++ api/ApiConnector.php
@@ -48,4 +48,38 @@ class ApiConnector
 
         $responseData = json_decode($response, true);
     }
+
+    public function getArticlesUpdatedSince(string $dateTime): array 
+    {
+        $url = $this->baseUrl . '/articles?updated_since=' . urlencode($dateTime);
+
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+        ];
+
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            throw new Exception('Erreur CURL : ' . curl_error($ch));
+        }
+
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpCode !== 200) {
+            throw new Exception('Erreur API lors de la récupération des articles. Code HTTP : ' . $httpCode);
+        }
+
+        $articles = json_decode($response, true);
+
+        if (!is_array($articles)) {
+            throw new Exception('Format de réponse inattendu lors de la récupération des articles.');
+        }
+
+        return $articles;
+    }
 }
