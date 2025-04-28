77f2c1af56fa840e5316df86beefebc84ccba1a6
Darhaf <fhv.huseynov@gmail.com>
2025-04-29 00:04:35 +0200

    création fichier apiconnector.php

---
+++ api/ApiConnector.php
@@ -0,0 +1,51 @@
+<?php
+
+class ApiConnector
+{
+    private string $baseUrl;
+    private string $login;
+    private string $password;
+
+    public function __construct(string $baseUrl, string $login, string $password)
+    {
+        $this->baseUrl  = $baseUrl;
+        $this->login    = $login;
+        $this->password = $password;
+        $this->authenticate();
+    }
+
+    private function authenticate(): void
+    {
+        $url = $this->baseUrl;
+        
+        $data = [
+            'login' => $this->login,
+            'password' => $this->password
+        ];
+
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_POST => true,
+            CURLOPT_RETURNTRANSFER => true, 
+            CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+            CURLOPT_POSTFIELDS => json_encode($data),
+        ];
+
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            throw new Exception('Erreur CURL : ' . curl_error($ch));
+        }
+
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpCode !== 200) {
+            throw new Exception("Erreur API lors de l'authentification. Code HTTP : " . $httpCode);
+        }
+
+        $responseData = json_decode($response, true);
+    }
+}
